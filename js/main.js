b22feed2d605b5ad1f03a893284b8ec13b523526
Alvaro Sanz Garcia <fe7575qi@go.minnstate.edu>
2025-12-18 12:13:42 -0600

    fConvert SplitSquare to installable Progressive Web App

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@
 
 
 
+// PWA Service Worker registration
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(reg => console.log('Service worker registered:', reg))
+    .catch(err => console.error('Service worker registration failed:', err));
+}
+
 // Variable declaration
 const group = [];
 const form = document.getElementById("participant-form");
