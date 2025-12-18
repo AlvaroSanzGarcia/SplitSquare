83783dcb82fb9f527d40f7e2f4064ddb9f83c596
Alvaro Sanz Garcia <fe7575qi@go.minnstate.edu>
2025-12-18 12:50:59 -0600

    Updated service worker

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,25 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+const CACHE_NAME = 'splitsquare-v1';
+const urlsToCache = [
+  '/',
+  '/SplitSquare/index.html',
+  '/SplitSquare/css/index_styles.css',
+  '/SplitSquare/js/main.js',
+  '/SplitSquare/img/SplitSquareLogo.png',
+  '/SplitSquare/manifest.json'
+];
 
-workbox.routing.registerRoute(
-	/\.(?:css|js|jsx|json)$/,
-	new workbox.strategies.StaleWhileRevalidate({
-		cacheName: 'assets',
-		plugins: [
-			new workbox.expiration.ExpirationPlugin({
-				maxEntries: 1000,
-				maxAgeSeconds: 31536000
-			})
-		]
-	})
-);
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
-workbox.routing.registerRoute(
-	/\.(?:png|jpg|jpeg|gif|woff2)$/,
-	new workbox.strategies.CacheFirst({
-		cacheName: 'images',
-		plugins: [
-			new workbox.expiration.ExpirationPlugin({
-				maxEntries: 1000,
-				maxAgeSeconds: 31536000
-			})
-		]
-	})
-);
-
-workbox.routing.registerRoute(
-	/(\/)$/,
-	new workbox.strategies.StaleWhileRevalidate({
-		cacheName: 'startPage',
-		plugins: [
-			new workbox.expiration.ExpirationPlugin({
-				maxEntries: 1000,
-				maxAgeSeconds: 31536000
-			})
-		]
-	})
-);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
